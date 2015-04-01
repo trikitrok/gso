3efa30d1aece88a17bfdd1b7aa720600cc0a9ec9
trikitrok <trikitrok@gmail.com>
2015-04-02 00:17:58 +0200

    Aliased namespaces with shorter names

--- test/gso/glowworm_test.clj
+++ test/gso/glowworm_test.clj
@@ -1,13 +1,14 @@
 (ns gso.glowworm-test
   (:use midje.sweet)
-  (:use [gso.glowworm])
-  (:require [gso.objective-functions]))
+  (:use [gso.glowworm :as glowworm])
+  (:require [gso.objective-functions :as obj-fns]))
 
 (facts
   "about a glowworm"
   
   (fact 
-    "it computes it luciferin using an objective function (J1)"
-    (let [g (gso.glowworm/make [0.0 0.0] {:gamma 0.6 :rho 0.4} 5.0)]
-      (luciferin g gso.objective-functions/j1) => (+ (* 5.0 (- 1.0 0.4))
-                                                     (* 0.6 0.9810118431238463)))))
+    "it computes its luciferin using an objective function (J1 in this case)"
+    (let [g (glowworm/make 
+              [0.0 0.0] {:gamma 0.6 :rho 0.4} 5.0)]
+      (luciferin g obj-fns/j1) => (+ (* 5.0 (- 1.0 0.4))
+                                     (* 0.6 0.9810118431238463)))))
