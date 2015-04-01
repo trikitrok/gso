e95eb099df2fd9cf018566dd9608909ef631bd44
trikitrok <trikitrok@gmail.com>
2015-04-01 23:35:07 +0200

    J3 benchmark function working

--- src/gso/objective_functions.clj
+++ src/gso/objective_functions.clj
@@ -5,10 +5,12 @@
      (* 10.0 (- (/ x 5.0) (Math/pow x 3) (Math/pow y 5)) (Math/exp (- (+ (* x x) (* y y)))))
      (* (/ 1 3.0) (Math/exp (- (+ (* (+ x 1) (+ x 1)) (* y y)))))))
 
-
-;; 20.0 + (x * x - 10.0 * cos(2 * M_PI * x) + y * y - 10.0 * cos(2 * M_PI * y))
 (defn j2 [[x y]]
   (+ 20.0 (+ (* x x) 
              (* (- 10.0) (Math/cos (* 2 Math/PI x)))
              (* y y)
              (* (- 10.0) (Math/cos (* 2 Math/PI y))))))
+
+(defn j3 [[x y]]
+  (* (Math/pow (+ (* x x) (* y y)) 0.25)
+     (+ 1.0 (Math/pow (Math/sin (* 50.0 (Math/pow (+ (* x x) (* y y)) 0.1))) 2.0))))
