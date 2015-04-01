8d7a0adb8c2bac69c0a5debc4d0ba96b6f29f08b
trikitrok <trikitrok@gmail.com>
2015-04-01 23:13:42 +0200

    J2 benchmark function working

--- src/gso/objective_functions.clj
+++ src/gso/objective_functions.clj
@@ -4,3 +4,11 @@
   (- (* 3.0 (- 1 x) (- 1 x) (Math/exp (- (+ (* x x) (* (+ y 1) (+ y 1))))))
      (* 10.0 (- (/ x 5.0) (Math/pow x 3) (Math/pow y 5)) (Math/exp (- (+ (* x x) (* y y)))))
      (* (/ 1 3.0) (Math/exp (- (+ (* (+ x 1) (+ x 1)) (* y y)))))))
+
+
+;; 20.0 + (x * x - 10.0 * cos(2 * M_PI * x) + y * y - 10.0 * cos(2 * M_PI * y))
+(defn j2 [[x y]]
+  (+ 20.0 (+ (* x x) 
+             (* (- 10.0) (Math/cos (* 2 Math/PI x)))
+             (* y y)
+             (* (- 10.0) (Math/cos (* 2 Math/PI y))))))
