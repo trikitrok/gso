e36f9ca0334e34f764b4bb64725bfb7906383b0e
trikitrok <trikitrok@gmail.com>
2015-04-02 01:49:43 +0200

    Extracted a helper and used vec-diff inside squared distance

--- src/gso/vector_functions.clj
+++ src/gso/vector_functions.clj
@@ -1,10 +1,13 @@
 (ns gso.vector-functions)
 
-(defn squared-dist [v1 v2]
-  (reduce + (map (comp #(* % %) -) v1 v2)))
+(defn- square [a]
+  (* a a))
 
 (defn vec-diff [v1 v2]
   (map - v1 v2))
+
+(defn squared-dist [v1 v2]
+  (reduce + (map square (vec-diff v1 v2))))
 
 (defn norm [v1]
   (Math/sqrt (squared-dist v1 v1)))
