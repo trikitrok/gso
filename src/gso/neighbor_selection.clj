18dc68334aae7760749ebd829385e0500bffbf1b
trikitrok <trikitrok@gmail.com>
2015-04-18 22:55:16 +0200

    Selecting a neighbor to move towards

---
+++ src/gso/neighbor_selection.clj
@@ -0,0 +1,21 @@
+(ns gso.neighbor-selection)
+
+(defn compute-probabilities [neighbors {luciferin-glowworm :luciferin}]
+  (let [luciferin-diffs (map #(- (:luciferin %) luciferin-glowworm) neighbors)
+        sum-diffs (reduce + luciferin-diffs)]
+    (map #(/ % sum-diffs) luciferin-diffs)))
+
+(defn compute-accumulated-probabilities [probabilities]
+  (map #(reduce + %)
+       (for [n (range 1 (inc (count probabilities)))]
+         (take n probabilities))))
+
+(defn get-neighbor-index [probabilities rand-fn]
+  (let [rnd (rand-fn)
+        accumulated-probabilities (compute-accumulated-probabilities probabilities)]
+    (count (take-while #(< % rnd) accumulated-probabilities))))
+
+(defn select-one-towards-to-move [neighbors glowworm rand-fn]
+  (let [probabilities (compute-probabilities neighbors glowworm)
+        neighbor-index (get-neighbor-index probabilities rand-fn)]
+    (nth neighbors neighbor-index)))
---
+++ test/gso/neighbor_selection_test.clj
@@ -0,0 +1,36 @@
+(ns gso.neighbor-selection-test
+  (:use midje.sweet)
+  (:use [gso.glowworm :as glowworm])
+  (:use [gso.neighbor-selection :as neig-select]))
+
+(facts
+  "about neighbors selection"
+  (let [g1 (glowworm/make
+             :coords [0.0 0.0]
+             :params {:gamma 0.6 :rho 0.4 :beta 0.08
+                      :maximum-neighbors 5 :maximum-vision-range 5.0}
+             :luciferin 3.0
+             :vision-range 1.0)
+        g2 (glowworm/make
+             :coords [0.1 0.0]
+             :params {:gamma 0.6 :rho 0.4 :beta 0.08
+                      :maximum-neighbors 5 :maximum-vision-range 5.0}
+             :luciferin 5.0
+             :vision-range 1.0)
+        g3 (glowworm/make
+             :coords [0.0 0.1]
+             :params {:gamma 0.6 :rho 0.4 :beta 0.08
+                      :maximum-neighbors 5 :maximum-vision-range 5.0}
+             :luciferin 4.0
+             :vision-range 1.0)
+        g4 (glowworm/make
+             :coords [0.2 0.0]
+             :params {:gamma 0.6 :rho 0.4 :beta 0.08
+                      :maximum-neighbors 5 :maximum-vision-range 5.0}
+             :luciferin 6.0
+             :vision-range 1.0)
+        neighbors [g2 g3 g4]]
+
+    (neig-select/select-one-towards-to-move neighbors g1 (constantly 0.55)) => g4
+    (neig-select/select-one-towards-to-move neighbors g1 (constantly 0.1)) => g2
+    (neig-select/select-one-towards-to-move neighbors g1 (constantly 0.35)) => g3))
