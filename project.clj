17b6958bd3db6dd7fa77873ebf6036fd51a67b69
trikitrok <trikitrok@gmail.com>
2015-06-06 18:37:36 +0200

    Added Sean Luke's Mersenne Twister random numbers generator implementation in Java and rearranged project directories to have both java and clojure code

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [dire "0.5.3"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
+  :source-paths      ["src/clojure"]
+  :java-source-paths ["src/java"])
---
+++ test/gso/random_generation_test.clj
@@ -0,0 +1,58 @@
+(ns gso.random-generation-test
+  (:use midje.sweet)
+  (:require [gso.random-generation :as rng-fns]))
+
+(defn in-range? [lower-limit upper-limit num]
+  (and (>= num lower-limit) (< num upper-limit)))
+
+(facts
+  "about MersenneTwister generator"
+
+  (fact
+    "using the same seed equal random numbers sequences are generated"
+    (let [seed 4357
+          rng1 (rng-fns/make-mersenne-twister-rng seed)
+          rng2 (rng-fns/make-mersenne-twister-rng seed)
+          rand-sequence-1 (take 1000 (repeatedly #(rng-fns/double-in-0-1! rng1)))
+          rand-sequence-2 (take 1000 (repeatedly #(rng-fns/double-in-0-1! rng2)))]
+      rand-sequence-1 => rand-sequence-2))
+
+  (fact
+    "using different seeds different random numbers sequences are generated"
+    (let [rng1 (rng-fns/make-mersenne-twister-rng 4357)
+          rng2 (rng-fns/make-mersenne-twister-rng 4358)
+          rand-sequence-1 (take 1000 (repeatedly #(rng-fns/double-in-0-1! rng1)))
+          rand-sequence-2 (take 1000 (repeatedly #(rng-fns/double-in-0-1! rng2)))]
+      rand-sequence-1 =not=> rand-sequence-2))
+
+  (fact
+    "double-in-0-1! produces numbers in range [0, 1)"
+    (let [rng (rng-fns/make-mersenne-twister-rng 4357)
+          in-range-0-1? (partial in-range? 0 1)
+          rand-sequence (take 1000 (repeatedly #(rng-fns/double-in-0-1! rng)))]
+      rand-sequence => (has every? in-range-0-1?)))
+
+  (fact
+    "double-in-range! produces numbers in range [lower-limit, upper-limit)"
+    (let [rng (rng-fns/make-mersenne-twister-rng 4357)
+          lower-limit 5
+          upper-limit 10
+          in-range? (partial in-range? lower-limit upper-limit)
+          rand-sequence (take 1000 (repeatedly #(rng-fns/double-in-range! rng lower-limit upper-limit)))]
+      rand-sequence => (has every? in-range?)))
+
+  (fact
+    "select-random-element! gets a random element from a sequence"
+    (let [rng (rng-fns/make-mersenne-twister-rng 4357)
+          elements [1 3 5]]
+      (rng-fns/select-random-element! rng elements) => 3
+      (rng-fns/select-random-element! rng elements) => 1
+      (rng-fns/select-random-element! rng elements) => 1
+      (rng-fns/select-random-element! rng elements) => 1
+      (rng-fns/select-random-element! rng elements) => 3
+      (rng-fns/select-random-element! rng elements) => 3
+      (rng-fns/select-random-element! rng elements) => 3
+      (rng-fns/select-random-element! rng elements) => 5
+      (rng-fns/select-random-element! rng elements) => 3
+      (rng-fns/select-random-element! rng elements) => 1
+      (rng-fns/select-random-element! rng elements) => 1)))
