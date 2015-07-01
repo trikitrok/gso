d100ea2b729f1a7723c5b167e2d327e670c0cf58
trikitrok <trikitrok@gmail.com>
2015-07-01 15:34:38 +0200

    Renamed function

--- src/clojure/gso/neighbor_selection.clj
+++ src/clojure/gso/neighbor_selection.clj
@@ -1,21 +1,25 @@
 (ns gso.neighbor-selection)
 
-(defn- compute-probabilities-of-selecting [neighbors {luciferin-glowworm :luciferin}]
+(defn- compute-probabilities-of-selecting
+  [neighbors {luciferin-glowworm :luciferin}]
   (let [luciferin-diffs (map #(- (:luciferin %) luciferin-glowworm) neighbors)
         sum-diffs (reduce + luciferin-diffs)]
     (map #(/ % sum-diffs) luciferin-diffs)))
 
-(defn- compute-accumulated-probabilities [probabilities]
+(defn- compute-accumulated-probabilities
+  [probabilities]
   (map #(reduce + %)
        (for [n (range 1 (inc (count probabilities)))]
          (take n probabilities))))
 
-(defn- get-index-randomly [accumulated-probabilities rnd]
-  (count (take-while #(< % rnd) accumulated-probabilities)))
+(defn- get-index-randomly
+  [accumulated-probabilities random-number]
+  (count (take-while #(< % random-number) accumulated-probabilities)))
 
-(defn select-neighbor [rand-fn glowworm neighbors]
+(defn select-neighbor
+  [double-in-0-1! glowworm neighbors]
   (nth neighbors
        (get-index-randomly
          (compute-accumulated-probabilities
            (compute-probabilities-of-selecting neighbors glowworm))
-         (rand-fn))))
+         (double-in-0-1!))))
