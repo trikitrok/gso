70b97b5fdb3592d5c8fe0560e124ff3b9742eca8
trikitrok <trikitrok@gmail.com>
2015-07-01 15:28:35 +0200

    Introduced a protocol for Random numbers generation

--- src/clojure/gso/random_generation.clj
+++ src/clojure/gso/random_generation.clj
@@ -1,30 +1,36 @@
 (ns gso.random-generation
   (:import (random_generation MersenneTwister)))
 
+(defprotocol RandomGeneration
+  (double-in-range! [this lower-limit upper-limit]
+    "On each invocation, it returns a random floating-point value
+     uniformly distributed in the range [lower-limit, upper-limit).
+     It mutates the rng field")
+
+  (double-in-0-1! [this]
+    "On each invocation, it returns a random floating-point value
+    uniformly distributed in the range [0, 1).
+    It mutates the rng field.")
+
+  (select-random-element! [this elements]
+    "On each invocation, it randomly selects and returns
+    an element from the elements sequence.
+    It mutates the rng field"))
+
+(defrecord MersenneTwisterRandomGenerator [rng]
+  RandomGeneration
+  (double-in-range!
+    [this lower-limit upper-limit]
+    (+ lower-limit (* (- upper-limit lower-limit) (double-in-0-1! this))))
+
+  (double-in-0-1!
+    [this]
+    (.nextDouble (:rng this) true true))
+
+  (select-random-element!
+    [this elements]
+    (nth elements (.nextInt (:rng this) (count elements)))))
+
 (defn make-mersenne-twister-rng [seed]
-  (MersenneTwister. seed))
-
-(defn double-in-0-1!
-  "On each invocation, it returns a random floating-point value
-  uniformly distributed in the range [0, 1).
-  It mutates the rng argument."
-  [rng]
-  (.nextDouble rng true true))
-
-(defn double-in-range!
-  "On each invocation, it returns a random floating-point value
-  uniformly distributed in the range [lower-limit, upper-limit).
-  It mutates the rng argument"
-  [rng lower-limit upper-limit]
-  (+ lower-limit (* (- upper-limit lower-limit) (double-in-0-1! rng))))
-
-(defn- int-under-n!
-  "On each invocation, it returns an integer value between 0 and n-1.
-  It mutates the rng argument"
-  [rng n]
-  (.nextInt rng n))
-
-(defn select-random-element! [rng elements]
-  "On each invocation, it randomly selects and returns an element from the elements sequence.
-  It mutates the rng argument"
-  (nth elements (int-under-n! rng (count elements))))
+      (MersenneTwisterRandomGenerator.
+        (MersenneTwister. seed)))
