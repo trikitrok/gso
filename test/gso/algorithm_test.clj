4b69aebb747280967b52a3debe060ffbf0cfc231
trikitrok <trikitrok@gmail.com>
2015-07-09 17:50:36 +0200

    Algorithm finding all local minima for J2 objective function

--- test/gso/algorithm_test.clj
+++ test/gso/algorithm_test.clj
@@ -8,7 +8,7 @@
             [gso.test-helpers :as test-helpers]))
 
 (facts
-  "About algorithm"
+  "About GSO algorithm"
 
   (fact
     "it finds the local minima when using J1 objective function"
@@ -28,9 +28,9 @@
 
        rng (rng-fns/make-mersenne-twister-rng seed)
 
-       swarm-size 50
+       swarm-size 55
 
-       num-generations 250
+       num-generations 150
 
        box [{:min -3 :max 3}
             {:min -3 :max 3}]
@@ -51,4 +51,47 @@
        expected-local-minima [[1.28, 0.0] [0.0, 1.58] [-0.46, -0.63]]]
 
       (test-helpers/all-local-minima-found?
-        expected-local-minima 3 0.05 last-generation) => true)))
+        expected-local-minima 3 0.05 last-generation) => true))
+
+  (fact
+    "it finds the local minima when using J2 objective function"
+    (let
+      [params
+       {:gamma                0.6
+        :rho                  0.4
+        :beta                 0.08
+        :maximum-neighbors    5
+        :maximum-vision-range 2.0}
+
+       initial-values
+       {:vision-range 2.0
+        :luciferin    5.0}
+
+       seed 324324
+
+       rng (rng-fns/make-mersenne-twister-rng seed)
+
+       swarm-size 70
+
+       num-generations 150
+
+       box [{:min -1 :max 1}
+            {:min -1 :max 1}]
+
+       initial-swarm
+       (swarm-factory/create-random-swarm
+         swarm-size initial-values params rng box)
+
+       gso-algorithm
+       (gso-algorithm/make
+         neighbors/search-neighbors
+         (partial rng-fns/double-in-0-1! rng)
+         objective-fns/j2)
+
+       last-generation
+       (last (gso-algorithm num-generations initial-swarm))
+
+       expected-local-minima [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]]]
+
+      (test-helpers/all-local-minima-found?
+        expected-local-minima 5 0.05 last-generation) => true)))
