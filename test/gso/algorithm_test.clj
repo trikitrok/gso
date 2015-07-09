b04e9f52f6b047091d7d0ad568d71448b8faaeca
trikitrok <trikitrok@gmail.com>
2015-07-09 17:05:37 +0200

    Algorithm finding all local minima for J1 objective function

---
+++ test/gso/algorithm_test.clj
@@ -0,0 +1,54 @@
+(ns gso.algorithm-test
+  (:use midje.sweet)
+  (:require [gso.algorithm :as gso-algorithm]
+            [gso.neighbors-search :as neighbors]
+            [gso.random-generation :as rng-fns]
+            [gso.objective-functions :as objective-fns]
+            [gso.swarm-factory :as swarm-factory]
+            [gso.test-helpers :as test-helpers]))
+
+(facts
+  "About algorithm"
+
+  (fact
+    "it finds the local minima when using J1 objective function"
+    (let
+      [params
+       {:gamma                0.6
+        :rho                  0.4
+        :beta                 0.08
+        :maximum-neighbors    5
+        :maximum-vision-range 5.0}
+
+       initial-values
+       {:vision-range 3.0
+        :luciferin    5.0}
+
+       seed 324324
+
+       rng (rng-fns/make-mersenne-twister-rng seed)
+
+       swarm-size 50
+
+       num-generations 250
+
+       box [{:min -3 :max 3}
+            {:min -3 :max 3}]
+
+       initial-swarm
+       (swarm-factory/create-random-swarm
+         swarm-size initial-values params rng box)
+
+       gso-algorithm
+       (gso-algorithm/make
+         neighbors/search-neighbors
+         (partial rng-fns/double-in-0-1! rng)
+         objective-fns/j1)
+
+       last-generation
+       (last (gso-algorithm num-generations initial-swarm))
+
+       expected-local-minima [[1.28, 0.0] [0.0, 1.58] [-0.46, -0.63]]]
+
+      (test-helpers/all-local-minima-found?
+        expected-local-minima 3 0.05 last-generation) => true)))
--- test/gso/test_helpers.clj
+++ test/gso/test_helpers.clj
@@ -6,3 +6,19 @@
 
 (defn in-range? [lower-limit upper-limit num]
   (and (>= num lower-limit) (< num upper-limit)))
+
+(defn- nearly-equal? [actual expected tol]
+  (every? #(> tol %)
+          (map #(Math/abs (- %1 %2)) actual expected)))
+
+(defn- matches [min-coords generation tol]
+  (filter #(nearly-equal? min-coords (:coords %) tol)
+          generation))
+
+(defn- local-minimum-found? [min-matches tol generation min-coords]
+  (<= min-matches (count (matches min-coords generation tol))))
+
+(defn all-local-minima-found? [mins-coords min-matches tol generation]
+  (every?
+    (partial local-minimum-found? min-matches tol generation)
+    mins-coords))
