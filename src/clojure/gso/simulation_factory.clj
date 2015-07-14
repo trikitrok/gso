5e4567f580cbb4d0146d9effca5f7132b3daf5f5
trikitrok <trikitrok@gmail.com>
2015-07-14 13:30:37 +0200

    Added a method to create a gso simulation from a configuration map

---
+++ src/clojure/gso/simulation_factory.clj
@@ -0,0 +1,29 @@
+(ns gso.simulation-factory
+  (:require [gso.algorithm :as gso-algorithm]
+            [gso.neighbors-search :as neighbors]
+            [gso.random-generation :as rng-fns]
+            [gso.objective-functions :as objective-fns]
+            [gso.swarm-factory :as swarm-factory]))
+
+(def obj-fns-by-key
+  {:j1 objective-fns/j1
+   :j2 objective-fns/j2
+   :j3 objective-fns/j3})
+
+(defn make-gso-simulation
+  [{:keys [params initial-values random-seed swarm-size num-generations obj-fn boxes]}]
+  (let [rng (rng-fns/make-mersenne-twister-rng random-seed)
+
+        obj-fn (obj-fns-by-key obj-fn)
+
+        initial-swarm
+        (swarm-factory/create-random-swarm
+          swarm-size initial-values params rng boxes)
+
+        gso-algorithm
+        (gso-algorithm/make
+          neighbors/search-neighbors
+          (partial rng-fns/double-in-0-1! rng)
+          obj-fn)]
+
+    #(gso-algorithm num-generations initial-swarm)))
--- test/gso/algorithm_test.clj
+++ test/gso/algorithm_test.clj
@@ -5,93 +5,56 @@
             [gso.random-generation :as rng-fns]
             [gso.objective-functions :as objective-fns]
             [gso.swarm-factory :as swarm-factory]
+            [gso.simulation-factory :as simulation-factory]
             [gso.test-helpers :as test-helpers]))
+
+(defn- correct-simulation-results?
+  [run-gso-generations expected-local-minima min-matches tol]
+  (let [last-generation (last (run-gso-generations))]
+    (test-helpers/all-local-minima-found?
+      expected-local-minima min-matches tol last-generation)))
 
 (facts
   "About GSO algorithm"
 
   (fact
     "it finds the local minima when using J1 objective function"
-    (let
-      [params
-       {:gamma                0.6
-        :rho                  0.4
-        :beta                 0.08
-        :maximum-neighbors    5
-        :maximum-vision-range 5.0}
-
-       initial-values
-       {:vision-range 3.0
-        :luciferin    5.0}
-
-       seed 324324
-
-       rng (rng-fns/make-mersenne-twister-rng seed)
-
-       swarm-size 55
-
-       num-generations 150
-
-       box [{:min -3 :max 3}
-            {:min -3 :max 3}]
-
-       initial-swarm
-       (swarm-factory/create-random-swarm
-         swarm-size initial-values params rng box)
-
-       gso-algorithm
-       (gso-algorithm/make
-         neighbors/search-neighbors
-         (partial rng-fns/double-in-0-1! rng)
-         objective-fns/j1)
-
-       last-generation
-       (last (gso-algorithm num-generations initial-swarm))
-
-       expected-local-minima [[1.28, 0.0] [0.0, 1.58] [-0.46, -0.63]]]
-
-      (test-helpers/all-local-minima-found?
-        expected-local-minima 3 0.05 last-generation) => true))
+    (correct-simulation-results?
+      (simulation-factory/make-gso-simulation
+        {:params          {:gamma                0.6
+                           :rho                  0.4
+                           :beta                 0.08
+                           :maximum-neighbors    5
+                           :maximum-vision-range 5.0}
+         :initial-values  {:vision-range 3.0
+                           :luciferin    5.0}
+         :random-seed     324324
+         :swarm-size      55
+         :num-generations 150
+         :boxes           [{:min -3 :max 3}
+                           {:min -3 :max 3}]
+         :obj-fn          :j1})
+      [[1.28, 0.0] [0.0, 1.58] [-0.46, -0.63]]
+      3
+      0.05) => true)
 
   (fact
     "it finds the local minima when using J2 objective function"
-    (let
-      [params
-       {:gamma                0.6
-        :rho                  0.4
-        :beta                 0.08
-        :maximum-neighbors    5
-        :maximum-vision-range 2.0}
-
-       initial-values
-       {:vision-range 2.0
-        :luciferin    5.0}
-
-       seed 324324
-
-       rng (rng-fns/make-mersenne-twister-rng seed)
-
-       swarm-size 70
-
-       num-generations 150
-
-       box [{:min -1 :max 1}
-            {:min -1 :max 1}]
-
-       initial-swarm
-       (swarm-factory/create-random-swarm
-         swarm-size initial-values params rng box)
-
-       gso-algorithm
-       (gso-algorithm/make
-         neighbors/search-neighbors
-         (partial rng-fns/double-in-0-1! rng)
-         objective-fns/j2)
-
-       last-generation
-       (last (gso-algorithm num-generations initial-swarm))
-
-       expected-local-minima [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]]]
-
-      (test-helpers/all-local-minima-found?
-        expected-local-minima 5 0.05 last-generation) => true)))
+    (correct-simulation-results?
+      (simulation-factory/make-gso-simulation
+        {:params          {:gamma                0.6
+                           :rho                  0.4
+                           :beta                 0.08
+                           :maximum-neighbors    5
+                           :maximum-vision-range 2.0}
+         :initial-values  {:vision-range 2.0
+                           :luciferin    5.0}
+         :random-seed     324324
+         :swarm-size      70
+         :num-generations 150
+         :boxes           [{:min -1 :max 1}
+                           {:min -1 :max 1}]
+         :obj-fn          :j2})
+      [[-0.5, -0.5], [-0.5, 0.5], [0.5, -0.5], [0.5, 0.5]]
+      5
+      0.05)) => true)
