d5ee8e05101de50f70c95d1feeda2e56d0efea0b
trikitrok <trikitrok@gmail.com>
2015-07-17 13:16:47 +0200

    Renamed some local bindings and created a helper to improve readability

--- test/gso/neighbor_selection_test.clj
+++ test/gso/neighbor_selection_test.clj
@@ -1,11 +1,15 @@
 (ns gso.neighbor-selection-test
   (:use midje.sweet)
   (:require [gso.glowworm :as glowworm]
-            [gso.neighbor-selection :refer [make-neighbor-selection-fn]]))
+            [gso.neighbor-selection :refer [make-neighbor-selection-fn]]
+            [gso.random-generation :refer [double-in-0-1! make-mersenne-twister-rng]]))
 
 (defn- neighbor-selection-using-random-number [random-number]
   (let [random-in-01-fn (constantly random-number)]
     (make-neighbor-selection-fn random-in-01-fn)))
+
+(defn- neighbor-selection-using-real-random-generator []
+  (make-neighbor-selection-fn rand))
 
 (facts
   "about neighbors selection"
@@ -19,9 +23,7 @@
             :luciferin 3.0
             :vision-range 1.0)
 
-          any-number 0.55
-
-          select-neighbor (make-neighbor-selection-fn (constantly any-number))]
+          select-neighbor (neighbor-selection-using-real-random-generator)]
 
       (select-neighbor a-glowworn []) => a-glowworn))
 
@@ -37,27 +39,27 @@
 
           neighbor1
           (glowworm/make
-               :coords [0.1 0.0]
-               :params {:gamma             0.6 :rho 0.4 :beta 0.08
-                        :maximum-neighbors 5 :maximum-vision-range 5.0}
-               :luciferin 5.0
-               :vision-range 1.0)
+            :coords [0.1 0.0]
+            :params {:gamma             0.6 :rho 0.4 :beta 0.08
+                     :maximum-neighbors 5 :maximum-vision-range 5.0}
+            :luciferin 5.0
+            :vision-range 1.0)
 
           neighbor2
           (glowworm/make
-               :coords [0.0 0.1]
-               :params {:gamma             0.6 :rho 0.4 :beta 0.08
-                        :maximum-neighbors 5 :maximum-vision-range 5.0}
-               :luciferin 4.0
-               :vision-range 1.0)
+            :coords [0.0 0.1]
+            :params {:gamma             0.6 :rho 0.4 :beta 0.08
+                     :maximum-neighbors 5 :maximum-vision-range 5.0}
+            :luciferin 4.0
+            :vision-range 1.0)
 
           neighrbor3
           (glowworm/make
-               :coords [0.2 0.0]
-               :params {:gamma             0.6 :rho 0.4 :beta 0.08
-                        :maximum-neighbors 5 :maximum-vision-range 5.0}
-               :luciferin 6.0
-               :vision-range 1.0)
+            :coords [0.2 0.0]
+            :params {:gamma             0.6 :rho 0.4 :beta 0.08
+                     :maximum-neighbors 5 :maximum-vision-range 5.0}
+            :luciferin 6.0
+            :vision-range 1.0)
 
           neighbors [neighbor1 neighbor2 neighrbor3]]
 
