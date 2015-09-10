e7411e1d5b375e288d3891b46f9d3546750c2e83
trikitrok <trikitrok@gmail.com>
2015-09-11 00:46:18 +0200

    Added end-to-end test to running simulations from the command line

--- src/clojure/gso/core.clj
+++ src/clojure/gso/core.clj
@@ -2,11 +2,15 @@
   (:require [gso.configuration :as configuration]
             [gso.simulation-factory :as simulation-factory]))
 
-(defn- save-result-in-file [result output-path]
+(defn- save-result-in-file [output-path result]
   (spit output-path (pr-str result)))
+
+(defn- create-simulation-from [config-file]
+  (-> config-file
+      configuration/load-from-file
+      simulation-factory/make-gso-simulation))
 
 (defn -main [& args]
   (let [[config-path result-path] args
-        config (configuration/load-from-file config-path)
-        simulation (simulation-factory/make-gso-simulation config)]
+        simulation (create-simulation-from config-path)]
     (save-result-in-file (simulation) result-path)))
--- test/gso/core_test.clj
+++ test/gso/core_test.clj
@@ -1,3 +1,18 @@
 (ns gso.core-test
   (:use midje.sweet)
-  (:use [gso.core]))
+  (:require [clojure.java.shell :refer [sh]]))
+
+(facts
+  "about running gso simulation from command line"
+
+  (let [expected-simulation-results-path "test/gso/expected-simulation-results.txt"
+        simulation-results-path "test/gso/simulation-results.txt"
+        exit-code (:exit (sh "lein" "run"
+                             "test/gso/simulation-config.txt"
+                             simulation-results-path))]
+    exit-code => 1
+    (slurp simulation-results-path) => (slurp expected-simulation-results-path)))
+
+
+
+
