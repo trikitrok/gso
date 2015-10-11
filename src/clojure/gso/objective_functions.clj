eccedaf9f382baca42248e32f1baf3fc14ebc525
trikitrok <trikitrok@gmail.com>
2015-10-11 22:20:02 +0200

    Reduced duplication and improved readability of objective functions

--- src/clojure/gso/objective_functions.clj
+++ src/clojure/gso/objective_functions.clj
@@ -1,16 +1,34 @@
 (ns gso.objective-functions)
 
+(defn- pow [x y] (Math/pow x y))
+
+(defn- sin [x] (Math/sin x))
+
+(defn- square [x] (* x x))
+
+(defn- sum-squares [& values]
+  (reduce + (map #(* % %) values)))
+
+(defn- inverse-exp-of-squared-sum [x y]
+  (Math/exp (- (sum-squares x y))))
+
+(def ^:private *-2-pi (partial * 2 Math/PI))
+
+(defn- j2-linear-term [x]
+  (* (- 10.0) (Math/cos (*-2-pi x))))
+
 (defn j1 [[x y]]
-  (- (* 3.0 (- 1 x) (- 1 x) (Math/exp (- (+ (* x x) (* (+ y 1) (+ y 1))))))
-     (* 10.0 (- (/ x 5.0) (Math/pow x 3) (Math/pow y 5)) (Math/exp (- (+ (* x x) (* y y)))))
-     (* (/ 1 3.0) (Math/exp (- (+ (* (+ x 1) (+ x 1)) (* y y)))))))
+  (- (* 3 (square (dec x)) (inverse-exp-of-squared-sum x (inc y)))
+     (* 10 (- (/ x 5.0) (pow x 3) (pow y 5)) (inverse-exp-of-squared-sum x y))
+     (* 1/3 (inverse-exp-of-squared-sum (inc x) y))))
 
 (defn j2 [[x y]]
-  (+ 20.0 (+ (* x x)
-             (* (- 10.0) (Math/cos (* 2 Math/PI x)))
-             (* y y)
-             (* (- 10.0) (Math/cos (* 2 Math/PI y))))))
+  (+ 20.0
+     (sum-squares x y)
+     (j2-linear-term x)
+     (j2-linear-term y)))
 
 (defn j3 [[x y]]
-  (* (Math/pow (+ (* x x) (* y y)) 0.25)
-     (+ 1.0 (Math/pow (Math/sin (* 50.0 (Math/pow (+ (* x x) (* y y)) 0.1))) 2.0))))
+  (let [squared-sum (sum-squares x y)]
+    (* (pow squared-sum 1/4)
+       (inc (square (sin (* 50.0 (pow squared-sum 1/10))))))))
